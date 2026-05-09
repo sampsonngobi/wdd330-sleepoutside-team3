275d06f2bb1bba9ad9c8ca34faff295ca6dcb1b8
dungasamsungo <sampsonngobi@gmail.com>
2026-05-09 17:42:24 -0300

    fixed the add to cart function.

--- src/js/product.js
+++ src/js/product.js
@@ -1,14 +1,37 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
+// function addProductToCart(product) {
+//   const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+//   cartItems.push(product);
+//   setLocalStorage("so-cart", cartItems);
+// }
+
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  if (!product) return;
+
+  const cartItems = getLocalStorage("so-cart") || [];
+
+  cartItems.push(product);
+
+  setLocalStorage("so-cart", cartItems);
 }
+
 // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
+
 async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
+  console.log(e.currentTarget.dataset.id);
+
+  const product = await dataSource.findProductById(e.currentTarget.dataset.id);
+
+  console.log(product);
+
   addProductToCart(product);
 }
 
